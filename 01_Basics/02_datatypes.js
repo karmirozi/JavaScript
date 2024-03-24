6b7a4fd6470b61b6f77a03031efc17625e0e8aa6
Rozi karmi <150064340+karmirozi@users.noreply.github.com>
2024-03-24 02:49:40 +0000

    datatypes

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -1,3 +1,34 @@
  "use strict";  //it means treat all JS code as newer version
 
- //alert("5 + 6") we are using NODE JS not browser  
+ //alert("5 + 6") we are using NODE JS not browser 
+ 
+
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("ROZY")
+
+
+let name = "rozy"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//NOTE:- KITNE TYPES K DATA TYPES HOTE HAI???
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => ye ek  standalone value hai
+// undefined => 
+// symbol => unique
+
+
+// WHAT IS OBJECT
+
+console.log(typeof undefined); // undefined(o/p)
+console.log(typeof null); // object(o/p)
